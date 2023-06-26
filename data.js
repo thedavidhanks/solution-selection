7849a7e88b074dd74be6542ad2d4041bd39be5e3
David Hanks <dphanks@gmail.com>
2023-06-26 11:52:38 -0400

    split html js css

---
+++ data.js
@@ -0,0 +1,104 @@
+const storageData = [
+    {
+        "Service": "AWS",
+        "Description": "Amazon Web Services offers a range of cloud-based storage and compute services to meet a variety of needs. Consult with Research IT Services at research-it@ucsd.edu for advice on the service that meets your needs and to discuss potential costs.",
+        "Capacity": "No overall limit (costs incurred).\nNo practical limit to number of files.",
+        "backups": true,
+        "Working Data": true,
+        "P1": true,
+        "P2": true,
+        "P3": true
+    },
+    {
+        "Service": "AWS Glacier",
+        "Description": "Amazon Glacier is a secure, durable, and extremely low-cost cloud storage service for data archiving and long-term backup. Amazon Glacier provides three options for access to archives, from a few minutes to several hours.",
+        "Capacity": "5 TB file size limit.\nNo overall limit (costs incurred).\nNo practical limit to number of files.",
+        "backups": true,
+        "Working Data": false,
+        "P1": true,
+        "P2": true,
+        "P3": false
+    },
+    {
+        "Service": "CTRI Storage",
+        "Description": "The Clinical Translational Research Institute (CTRI) helps researchers obtain the education, resources and collaborations necessary to translate discoveries into practice. The Institute is a partnership among the University of California, San Diego, and other local institutions dedicated to improving human health. To achieve this goal requires collaboration between many groups: academia, industry, non-profit agencies, government, and most importantly the community. Any researcher or individual interested in translational research from any group is invited to become a member of the CTRI.",
+        "Capacity": "Contact CTRI",
+        "backups": true,
+        "Working Data": true,
+        "P1": false,
+        "P2": false,
+        "P3": true
+    },
+    {
+        "Service": "Chronopolis",
+        "Description": "Digital preservation is a general term for a set of services with one goal: keeping your data safe so that it can be accessed in the future. Chronopolis is a non-commercial digital preservation network comprised of nonprofit higher education institutions and designed to ensure that your data is available beyond the life of a grant or research project.",
+        "Capacity": "No file size or number limit.\n\nOverall data limits determined on a case-by-case ba",
+        "backups": false,
+        "Working Data": false,
+        "P1": true,
+        "P2": true,
+        "P3": false
+    },
+    {
+        "Service": "Code42 CrashPlan",
+        "Description": "Protect and manage your institution’s most valuable resource—the data created and stored on devices.\n\nCode42 provides real-time recovery so no matter what happens, you always have access to your data. The beauty of our SaaS platform is that it also offers excellent data visibility for your security team, plus it saves IT a ton of time.",
+        "Capacity": "Contact vendor",
+        "backups": true,
+        "Working Data": false,
+        "P1": false,
+        "P2": false,
+        "P3": false
+    },
+    {
+        "Service": "Consult CTRI",
+        "Description": "The Clinical Translational Research Institute (CTRI) helps researchers obtain the education, resources and collaborations necessary to translate discoveries into practice. The Institute is a partnership among the University of California, San Diego, and other local institutions dedicated to improving human health. To achieve this goal requires collaboration between many groups: academia, industry, non-profit agencies, government, and most importantly the community. Any researcher or individual interested in translational research from any group is invited to become a member of the CTRI.",
+        "Capacity": "Varies",
+        "backups": false,
+        "Working Data": true,
+        "P1": false,
+        "P2": false,
+        "P3": false
+    },
+    {
+        "Service": "Consult Research IT Services",
+        "Description": "Reach out to Research IT Services at research-it@ucsd.edu for a personalized consultation.",
+        "Capacity": "",
+        "backups": true,
+        "Working Data": true,
+        "P1": true,
+        "P2": true,
+        "P3": true
+    },
+    {
+        "Service": "Consult local department/division IT",
+        "Description": "Ask your department or division IT staff for available options they may provide. Many, but not all, departments/divisions offer solutions tailored to their discipline areas.",
+        "Capacity": "",
+        "backups": true,
+        "Working Data": true,
+        "P1": true,
+        "P2": true,
+        "P3": true
+    },
+    {
+        "Service": "Druva InSync",
+        "Description": "Ensuring that your business data is protected is a huge challenge – especially with your data spread across endpoints and cloud applications like Office 365, Google G Suite, Box, and Salesforce. Druva inSync provides a simple approach to protecting, preserving and discovering your data, while reducing costs, risk and complexity.",
+        "Capacity": "Contact vendor",
+        "backups": true,
+        "Working Data": false,
+        "P1": false,
+        "P2": false,
+        "P3": false
+    },
+    {
+        "Service": "GitHub.com",
+        "Description": "GitHub Inc. is a web-based hosting service for version control using Git. It is mostly used for computer code. It offers all of the distributed version control and source code management (SCM) functionality of Git as well as adding its own features. It provides access control and several collaboration features such as bug tracking, feature requests, task management, and wikis for every project.",
+        "Capacity": "Limited, code",
+        "backups": false,
+        "Working Data": true,
+        "P1": true,
+        "P2": true,
+        "P3": false
+    }
+];
+
+export {storageData};
---
+++ datatable-rice.js
@@ -0,0 +1,159 @@
+import  {storageData} from './data.js';
+
+const createCompareTable = (newRowData) => {
+    const compareHeaderRow = $('#comparison-container thead tr');
+    const compareDescRow = $('tbody #compare-desc');
+    const compareCapacityRow = $('tbody #compare-capacity');
+    
+    //reset table
+    compareHeaderRow.empty();
+    compareDescRow.empty();
+    compareCapacityRow.empty();
+    $("<th>Service</th>").appendTo(compareHeaderRow);
+    $("<th>Description</th>").appendTo(compareDescRow);
+    $("<th>Capacity</th>").appendTo(compareCapacityRow);
+
+    //add new data
+    newRowData.forEach(row =>{
+        //console.log('row data',row);
+        let newHead = $("<th></th>").appendTo(compareHeaderRow);
+        newHead.html(row['Service']);
+        $("<td></td>").appendTo(compareDescRow).html(row['Description']);
+        $("<td></td>").appendTo(compareCapacityRow).html(row['Capacity']);
+    })
+}
+
+$(document).ready( function () {
+    let table = $('#myTable').DataTable(
+        {
+            data: storageData,
+            paging: false,
+            select: { style: true},
+            columns:[
+                {data: 'Service'},
+                {data: 'Description'},
+                {data: 'backups'},
+                {data: 'Working Data'},
+                {data: 'P1'},
+                {data: 'P2'},
+                {data: 'P3'},
+            ],
+            columnDefs:[
+                {
+                    target: 1,
+                    visible: false
+                },
+                {
+                    //backups, Working Data, P1, P2,P3
+                    targets: [2,3,4,5,6], 
+                    visible: false, 
+                    searchable: false, 
+                    searchPanes: { show: false}
+                },
+                /*{ configure just one column
+                    //Working Data
+                    target: 3, 
+                    visible: false, 
+                    searchable: false, 
+                    searchPanes: { show: false}
+                },*/
+            ],
+            searchPanes: {
+                cascadePanes: true,
+                //orderable: false,
+                layout: 'columns-1',
+                panes: [
+                    {
+                        header: 'Which storage need would you like to explore?',
+                        options: [
+                            {
+                                label: 'Backups',
+                                value: (rowData, rowIndex) =>  rowData['backups'] === true
+                            },
+                            {
+                                label: 'Working Data',
+                                value: (rowData, rowIndex) =>  rowData['Working Data'] === true
+                            }
+                        ],
+                        combiner: 'and'
+                    },
+                    {
+                        header: 'What is the UC protection level classification of your data?',
+                        options: [
+                            {
+                                label: 'P1 - Public',
+                                value: (rowData, rowIndex) =>  rowData['P1'] === true
+                            },
+                            {
+                                label: 'P2 - Internal',
+                                value: (rowData, rowIndex) =>  rowData['P2'] === true
+                            },
+                            {
+                                label: 'P3 - Human subject data',
+                                value: (rowData, rowIndex) =>  rowData['P3'] === true
+                            }
+                        ],
+                        combiner: 'and'
+                    }   
+                ],
+                controls: false,
+                
+            },
+            dom: '<"dtsp-dataTable"frtip>',
+            pageLength: 20
+        }
+    )
+    .on('select', function (e, dt, type, indexes) {
+        let newRowData = table.rows({selected: true}).data().toArray()
+        //var rowData = table.rows(indexes).data().toArray()
+
+        $('#row-data').html(JSON.stringify(newRowData))
+        .addClass("alert alert-secondary")
+        $('#selected-name').html(newRowData[0].Service);
+        $('#selected-description').html(newRowData[0].Description);
+
+        //Setup comparison table
+        if(newRowData.length == 1){
+            $('#selected-container').removeClass('hidden');
+            $('#comparison-container').addClass('hidden');
+        }
+        else if(newRowData.length > 1){
+            $('#selected-container').addClass('hidden');
+            $('#comparison-container').removeClass('hidden');
+            createCompareTable(newRowData);
+        }else{
+            $('#selected-container').addClass('hidden');
+            $('#comparison-container').addClass('hidden');
+        }
+    })
+    .on('deselect', function () {
+        //TODO regenerate comparison table with selected rows
+        let newRowData = table.rows({selected: true}).data().toArray()
+        if(newRowData.length == 1){
+            $('#selected-container').removeClass('hidden');
+            $('#comparison-container').addClass('hidden');
+        }
+        else if(newRowData.length > 1){
+            $('#selected-container').addClass('hidden');
+            $('#comparison-container').removeClass('hidden');
+            createCompareTable(newRowData);
+        }else{ //none selected
+            $('#selected-container').addClass('hidden');
+            $('#comparison-container').addClass('hidden');
+            $('#row-data').html('')
+            .removeClass("alert alert-secondary")
+            $('#selected-name').html('');
+            $('#selected-description').html('');
+        }
+        
+    })
+    
+    table.searchPanes()
+    $("div.dtsp-verticalPanes").append(table.searchPanes.container());                
+    
+    //Switch between table and cards
+    $("#card-toggler").click(()=>{
+        if($("#myTable").hasClass('cards')) $("#myTable").removeClass('cards');
+        else $("#myTable").addClass('cards');
+    })
+} );
